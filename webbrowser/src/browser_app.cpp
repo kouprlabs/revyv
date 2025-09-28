ecbdff4d5abb5f6f4639ad89070955473b60e6ba
Anass Bouassaba <ebouassaba@gmail.com>
2025-09-28 20:03:28 +0100

    Restore macOS keychain behavior for CEF

--- webbrowser/src/browser_app.cpp
+++ webbrowser/src/browser_app.cpp
@@ -1,8 +1,12 @@
 #include "browser_app.h"
 
+#include "include/base/cef_build.h"
+
 void BrowserApp::OnBeforeCommandLineProcessing(const CefString& process_type, CefRefPtr<CefCommandLine> command_line)
 {
-    command_line->AppendSwitch("disable-gpu");
-    command_line->AppendSwitch("disable-gpu-compositing");
-    command_line->AppendSwitch("enable-begin-frame-scheduling");
+#if defined(OS_MACOSX)
+    if (process_type.empty()) {
+        command_line->AppendSwitch("use-mock-keychain");
+    }
+#endif
 }
