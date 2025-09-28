70a9c63a263b3899239cd3b53e5ec674feaa4297
Anass Bouassaba <ebouassaba@gmail.com>
2025-09-28 19:27:29 +0100

    Load CEF framework in helper process on macOS

--- webbrowser/src/process_helper_mac.cpp
+++ webbrowser/src/process_helper_mac.cpp
@@ -1,7 +1,14 @@
 #include "include/cef_app.h"
+#include "include/wrapper/cef_library_loader.h"
 
 int main(int argc, char* argv[])
 {
     CefMainArgs main_args(argc, argv);
+#if defined(__APPLE__)
+    CefScopedLibraryLoader library_loader;
+    if (!library_loader.LoadInHelper()) {
+        return -1;
+    }
+#endif
     return CefExecuteProcess(main_args, nullptr, nullptr);
 }
